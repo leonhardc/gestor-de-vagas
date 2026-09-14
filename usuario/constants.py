84d8d9af14b5f4e2717e5797f32f214c7ac33ec2
Leonardo <leonardo99692370costa@gmail.com>
2026-09-14 14:53:38 -0300

    constantes do app usuario

---
+++ usuario/constants.py
@@ -0,0 +1,36 @@
+estado_civil_choices = [
+    ('solteiro', 'Solteiro'),
+    ('casado', 'Casado'),
+    ('divorciado', 'Divorciado'),
+    ('viuvo', 'Viúvo'),
+]
+
+escolaridade_choices = [
+    ('fundamental', 'Fundamental'),
+    ('medio', 'Médio'),
+    ('superior', 'Superior'),
+    ('pos_graduacao', 'Pós-Graduação'),
+]
+
+nacionalidade_choices = [
+    ('brasileiro', 'Brasileiro'),
+    ('estrangeiro', 'Estrangeiro'),
+]
+
+nivel_choices = [
+    ('iniciante', 'Iniciante'),
+    ('intermediario', 'Intermediário'),
+    ('avancado', 'Avançado'),
+]
+
+sexo_choices = [
+    ('masculino', 'Masculino'),
+    ('feminino', 'Feminino'),
+    ('outro', 'Outro'),
+]
+
+telefone_tipo_choices = [
+    ('celular', 'Celular'),
+    ('residencial', 'Residencial'),
+    ('comercial', 'Comercial'),
+]
