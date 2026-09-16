de24300bcde41ed88ecb220119fb88e836b5d506
Leonardo <leonardo99692370costa@gmail.com>
2026-09-16 14:56:44 -0300

    Adicionado constantes do app empresa

---
+++ empresa/constants.py
@@ -0,0 +1,30 @@
+modelo_de_trabalho_choices = [
+    ('presencial', 'Presencial'),
+    ('remoto', 'Remoto'),
+    ('hibrido', 'Híbrido'),
+]
+
+tipo_de_emprego_choices = [
+    ('estagio', 'Estágio'),
+    ('clt', 'CLT'),
+    ('pj', 'PJ'),
+    ('temporario', 'Temporário'),
+    ('freelance', 'Freelance'),
+]
+
+encontrado_em_choices = [
+    ('linkedin', 'LinkedIn'),
+    ('indeed', 'Indeed'),
+    ('glassdoor', 'Glassdoor'),
+    ('catho', 'Catho'),
+    ('vagas_com', 'Vagas.com'),
+    ('infojobs', 'InfoJobs'),
+    ('trampos_co', 'Trampos.co'),
+    ('outro', 'Outro'),
+]
+
+status_choices = [
+    ('pendente', 'Pendente'),
+    ('aprovado', 'Aprovado'),
+    ('rejeitado', 'Rejeitado'),
+]
